eeb63626a17c5988812678383a3d9bd158e16899
LinneaEO <olixl@wwu.edu>
2024-12-04 16:24:28 -0800

    Added some logic for gear icon button

--- AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Info.java
+++ AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Info.java
@@ -7,6 +7,8 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageButton;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,7 +16,7 @@ import android.view.ViewGroup;
  */
 public class Personal_Info extends Fragment {
 
-
+    private static EditText nameInput;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,21 @@ public class Personal_Info extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal__info, container, false);
+        View root = inflater.inflate(R.layout.fragment_personal__info, container, false);
+        ImageButton gearIcon = null;
+
+        Personal_Info.nameInput = root.findViewById(R.id.edittextnameid);
+        gearIcon = root.findViewById(R.id.geariconid);
+
+        nameInput.setEnabled(false);
+
+        gearIcon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                nameInput.setEnabled(true);
+            }
+        });
+        return root;
     }
 }
