f3eab6a0491be54c83b24953182708ac79077982
LinneaEO <olixl@wwu.edu>
2024-12-04 18:12:01 -0800

    Added enabling/disabling functionality for buttons

--- AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Info.java
+++ AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Info.java
@@ -1,5 +1,7 @@
 package com.example.alcoholconsumptiontracker;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -16,7 +18,11 @@ import android.widget.ImageButton;
  */
 public class Personal_Info extends Fragment {
 
-    private static EditText nameInput;
+    public static EditText nameInput;
+    public static EditText ageInput;
+    public static EditText sexInput;
+    public static EditText weightInput;
+    public static EditText heightInput;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -29,17 +35,53 @@ public class Personal_Info extends Fragment {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_personal__info, container, false);
         ImageButton gearIcon = null;
+        ImageButton finishIcon = null;
 
         Personal_Info.nameInput = root.findViewById(R.id.edittextnameid);
+        Personal_Info.ageInput = root.findViewById(R.id.edittextageid);
+        Personal_Info.sexInput = root.findViewById(R.id.edittextsexid);
+        Personal_Info.weightInput = root.findViewById(R.id.edittextweightid);
+        Personal_Info.heightInput = root.findViewById(R.id.edittextheightid);
+
+
+
         gearIcon = root.findViewById(R.id.geariconid);
+        finishIcon = root.findViewById(R.id.finishbuttonid);
 
         nameInput.setEnabled(false);
+        ageInput.setEnabled(false);
+        sexInput.setEnabled(false);
+        weightInput.setEnabled(false);
+        heightInput.setEnabled(false);
+
 
         gearIcon.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View view) {
                 nameInput.setEnabled(true);
+                ageInput.setEnabled(true);
+                sexInput.setEnabled(true);
+                weightInput.setEnabled(true);
+                heightInput.setEnabled(true);
+            }
+        });
+
+        finishIcon.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                nameInput.setEnabled(false);
+                ageInput.setEnabled(false);
+                sexInput.setEnabled(false);
+                weightInput.setEnabled(false);
+                heightInput.setEnabled(false);
+
+
+
+                String newName = String.valueOf(nameInput.getText());
+
+
             }
         });
         return root;
