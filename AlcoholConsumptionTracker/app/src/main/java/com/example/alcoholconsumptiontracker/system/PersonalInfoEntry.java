88ecda1aaf251e8ff3c42189f8b669de5f42e8c0
LinneaEO <olixl@wwu.edu>
2024-12-05 18:31:39 -0800

    Added this

--- AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/system/PersonalInfoEntry.java
+++ AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/system/PersonalInfoEntry.java
@@ -1,15 +1,17 @@
 package com.example.alcoholconsumptiontracker.system;
 
+import android.util.Log;
+
 import com.example.alcoholconsumptiontracker.Personal_Info;
 
 public class PersonalInfoEntry {
-    private static String userName;
-    private static int weight;
-    private static double height;
-    private static String sex;
-    private static int age;
+    private  String userName;
+    private  int weight;
+    private  double height;
+    private  String sex;
+    private  int age;
 
-    public void personalInfoEntry() {
+    public PersonalInfoEntry() {
         this.userName = null;
         this.weight = 0;
         this.height = 0.0;
@@ -20,41 +22,42 @@ public class PersonalInfoEntry {
     // Setterz and getterz
 
     // Getters - when u wanna know
-    public static String getUserName() {
+    public  String getUserName() {
         return userName;
     }
-    public static int getWeight() {
+    public  int getWeight() {
         return weight;
     }
-    public static double getHeight() {
+    public  double getHeight() {
         return height;
     }
-    public static String getSex() {
+    public  String getSex() {
         return sex;
     }
-    public static int getAge() {
+    public  int getAge() {
         return age;
     }
 
     // Setters - when u wanna change
-    public static void setUserName(String newUserName) {
-        PersonalInfoEntry.userName = newUserName;
+    public  void setUserName(String newUserName) {
+        this.userName = newUserName;
+        Log.d("PersonalInfoEntry", "UserName updated to: " + userName);
     }
 
-    public static void setWeight(int newWeight) {
-        PersonalInfoEntry.weight = newWeight;
+    public  void setWeight(int newWeight) {
+        this.weight = newWeight;
     }
 
-    public static void setHeight(int newHeight) {
-        PersonalInfoEntry.height = newHeight;
+    public  void setHeight(Double newHeight) {
+        this.height = newHeight;
     }
 
-    public static void setSex(String newSex) {
-        PersonalInfoEntry.sex = newSex;
+    public  void setSex(String newSex) {
+        this.sex = newSex;
     }
 
-    public static void setAge(int newAge) {
-        PersonalInfoEntry.age = newAge;
+    public  void setAge(int newAge) {
+        this.age = newAge;
     }
 
 }
