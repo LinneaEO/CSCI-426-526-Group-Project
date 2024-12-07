9a1c68def479edf13d2f4eb2e383e158340a0233
LinneaEO <olixl@wwu.edu>
2024-12-07 12:32:43 -0800

    Added all the initializations for EditText

--- AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Goals.java
+++ AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Personal_Goals.java
@@ -7,64 +7,26 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 public class Personal_Goals extends Fragment {
     // Constructor for personal goals - daily, monthly, weekly
-    public class personalGoal {
-        double goalPrice;
-        int goalServings;
-        int goalCalorie;
-        String goalWritten;
 
-        personalGoal(double goalPricePerson, int goalServings, int goalCalorie, String goalWritten) {
-            //this.goalPrice = goalPrice;
-            //this.goalServings = goalServings;
-            //this.goalCalorie = goalCalorie;
-            //this.goalWritten = goalWritten;
-            goalPrice = 0.00;
-            goalServings = 0;
-            goalCalorie = 0;
-            goalWritten = null;
-        }
+    public static EditText goalPriceDailyInput;
+    public static EditText goalServingsDailyInput;
+    public static EditText goalCalorieDailyInput;
+    public static EditText dailyWrittenGoals;
 
-        // Getters
-        public double getGoalPrice() {
-            return this.goalPrice;
-        }
-        public int getGoalServings() {
-            return this.goalServings;
-        }
-        public int getGoalCalorie() {
-            return this.goalCalorie;
-        }
-        public String getGoalWritten() {
-            return this.goalWritten;
-        }
+    public static EditText goalPriceWeeklyInput;
+    public static EditText goalServingsWeeklyInput;
+    public static EditText goalCalorieWeeklyInput;
+    public static EditText weeklyWrittenGoals;
 
-        // Setters - make em void cuz u dont wanna return anything
-        public void setGoalPrice(double newGoalPrice) {
-            this.goalPrice = newGoalPrice;
-        }
 
-        public void setGoalServings(int newGoalServings) {
-            this.goalServings = newGoalServings;
-        }
-
-        public void setGoalCalorie(int newGoalCaolorie) {
-            this.goalCalorie = newGoalCaolorie;
-        }
-
-        public void setGoalWritten(String newGoalWritten) {
-            this.goalWritten = newGoalWritten;
-        }
-    }
-
-    public static Personal_Goals newInstance(String param1, String param2) {
-        Personal_Goals fragment = new Personal_Goals();
-        Bundle args = new Bundle();
-        fragment.setArguments(args);
-        return fragment;
-    }
+    public static EditText goalPriceMonthlyInput;
+    public static EditText goalServingsMonthlyInput;
+    public static EditText goalCalorieMonthlyInput;
+    public static EditText monthlyWrittenGoals;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -75,6 +37,38 @@ public class Personal_Goals extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_personal__goals, container, false);
+        View root = inflater.inflate(R.layout.fragment_personal__goals, container, false);
+
+//        goalpricedailyid
+//                goalservingsdailyid
+//        goalcaloriesdailyid
+//                goalinfodailyid
+//        goalpriceweeklyid
+//                goalservingsweeklyid
+//        goalcaloriesweeklyid
+//                goalinfoweeklyid
+//        goalpricemonthlyid
+//                goalservingsmonthlyid
+//        goalcaloriesmonthlyid
+//                goalinfomonthlyid
+
+        goalPriceDailyInput = root.findViewById(R.id.goalpricedailyid);
+        goalServingsDailyInput = root.findViewById(R.id.goalservingsdailyid);
+        goalCalorieDailyInput = root.findViewById(R.id.goalcaloriesdailyid);
+        dailyWrittenGoals = root.findViewById(R.id.goalinfodailyid);
+
+        goalPriceWeeklyInput = root.findViewById(R.id.goalpriceweeklyid);
+        goalServingsWeeklyInput = root.findViewById(R.id.goalpriceweeklyid);
+        goalCalorieWeeklyInput = root.findViewById(R.id.goalpriceweeklyid);
+        weeklyWrittenGoals = root.findViewById(R.id.goalinfoweeklyid);
+
+        goalPriceMonthlyInput = root.findViewById(R.id.goalpricemonthlyid);
+        goalServingsMonthlyInput = root.findViewById(R.id.goalservingsmonthlyid);
+        goalCalorieMonthlyInput = root.findViewById(R.id.goalcaloriesmonthlyid);
+        monthlyWrittenGoals = root.findViewById(R.id.goalinfomonthlyid);
+
+
+
+        return root;
     }
 }
