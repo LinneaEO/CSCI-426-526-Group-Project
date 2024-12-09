2c1620e4f27ed74acdb0e825331f3c5315ccacad
Caroline Kays <caroline.kays@live.com>
2024-12-08 19:38:45 -0800

    before merge

--- AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Monthly_View.java
+++ AlcoholConsumptionTracker/app/src/main/java/com/example/alcoholconsumptiontracker/Monthly_View.java
@@ -11,8 +11,11 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewOutlineProvider;
 import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -25,13 +28,17 @@ import java.util.Locale;
  */
 public class Monthly_View extends Fragment {
 
+    private ImageView imageViewDataType;
     private TextView textViewMonth;
     private GridLayout gridLayoutCalendar;
-    private Button buttonCalories, buttonUnits, buttonBAC, buttonMoney;
+
+    private TextView totalTextView;
+    private ImageButton buttonCalories, buttonUnits, buttonBAC, buttonMoney;
     private String activeDataset = "calories";
 
     public Monthly_View() {
         // Required empty public constructor
+        super(R.layout.fragment_monthly__view);
     }
 
     /**
@@ -48,11 +55,13 @@ public class Monthly_View extends Fragment {
         return fragment;
     }
 
+    /*
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
     }
+    */
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -60,8 +69,11 @@ public class Monthly_View extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_monthly__view, container, false);
 
+        imageViewDataType = view.findViewById(R.id.imageViewDataType_m);
+
         // Find views
         TextView textViewMonth = view.findViewById(R.id.textViewMonth);
+        totalTextView = view.findViewById(R.id.total_value);
         gridLayoutCalendar = view.findViewById(R.id.gridLayoutCalendar);
         buttonCalories = view.findViewById(R.id.button_calories_m);
         buttonUnits = view.findViewById(R.id.button_units_m);
@@ -74,26 +86,31 @@ public class Monthly_View extends Fragment {
 
         // Default table view
         populateCalendar(getCaloriesData(), buttonCalories);
+        updateImageView(R.drawable.totalcaloriesbar);
 
         // Button listeners
         buttonCalories.setOnClickListener(v -> {
             activeDataset = "calories";
             populateCalendar(getCaloriesData(), buttonCalories);
+            updateImageView(R.drawable.totalcaloriesbar);
         });
 
         buttonUnits.setOnClickListener(v -> {
             activeDataset = "units";
             populateCalendar(getUnitsData(), buttonUnits);
+            updateImageView(R.drawable.totalunitsbar);
         });
 
         buttonBAC.setOnClickListener(v -> {
             activeDataset = "bac";
             populateCalendar(getBACData(), buttonBAC);
+            updateImageView(R.drawable.totalbacbar);
         });
 
         buttonMoney.setOnClickListener(v -> {
             activeDataset = "money";
             populateCalendar(getMoneyData(), buttonMoney);
+            updateImageView(R.drawable.totalpricebar);
         });
 
         return view;
@@ -107,13 +124,13 @@ public class Monthly_View extends Fragment {
     }
 
     // Populate the calendar table with data
-    private void populateCalendar(float[] data, Button activeButton) {
+    private void populateCalendar(float[] data, ImageButton activeButton) {
         gridLayoutCalendar.removeAllViews(); // Clear existing cells
 
         Calendar calendar = Calendar.getInstance();
         int daysInMonth = calendar.getActualMaximum(Calendar.DAY_OF_MONTH);
         int cellWidth = 150;
-        int cellHeight = 150;
+        int cellHeight = 200;
 
         // Add cells to the GridLayout
         for (int day = 1; day <= daysInMonth; day++) {
@@ -121,30 +138,54 @@ public class Monthly_View extends Fragment {
             textViewCell.setText(String.valueOf(day) + "\n" + data[day - 1]);
             textViewCell.setPadding(12, 12, 12, 12);
             textViewCell.setGravity(Gravity.CENTER);
+            textViewCell.setAlpha(0f);
+            textViewCell.animate().alpha(1f).setDuration(500).start();
+
 
             GradientDrawable cellBackground = new GradientDrawable();
-            cellBackground.setColor(Color.WHITE); // Set background color
-            cellBackground.setStroke(2, Color.GRAY); // Add a gray border
-            cellBackground.setCornerRadius(8); // Rounded corners
-            textViewCell.setBackground(cellBackground);
 
 
-            // TODO: set threshold to goal value
-            if ((activeDataset.equals("calories") && data[day - 1] > 300) ||
-                    (activeDataset.equals("units") && data[day - 1] > 3) ||
-                    (activeDataset.equals("bac") && data[day - 1] > 0.08) ||
-                    (activeDataset.equals("money") && data[day - 1] > 20)) {
-                textViewCell.setBackgroundResource(R.drawable.red_cell_background);
-            } else if ((activeDataset.equals("calories") && data[day - 1] < 100) ||
-                    (activeDataset.equals("units") && data[day - 1] <= 1) ||
-                    (activeDataset.equals("bac") && data[day - 1] <= 0.02) ||
-                    (activeDataset.equals("money") && data[day - 1] <= 10)) {
-                textViewCell.setBackgroundResource(R.drawable.green_cell_background); // Highlight cell in green
+            // Highlight current day
+            if (day == Calendar.getInstance().get(Calendar.DAY_OF_MONTH)) {
+                textViewCell.setBackgroundResource(R.drawable.current_cell_background);
+                cellBackground.setStroke(3, Color.GRAY);
             } else {
-                textViewCell.setBackgroundResource(R.drawable.cell_background);
+
+                // TODO: set threshold to goal value
+                if ((activeDataset.equals("calories") && data[day - 1] > 300) ||
+                        (activeDataset.equals("units") && data[day - 1] > 3) ||
+                        (activeDataset.equals("bac") && data[day - 1] > 0.08) ||
+                        (activeDataset.equals("money") && data[day - 1] > 20)) {
+                    textViewCell.setBackgroundResource(R.drawable.red_cell_background); // Highlight cell in red
+                } else if ((activeDataset.equals("calories") && data[day - 1] < 100) ||
+                        (activeDataset.equals("units") && data[day - 1] <= 1) ||
+                        (activeDataset.equals("bac") && data[day - 1] <= 0.02) ||
+                        (activeDataset.equals("money") && data[day - 1] <= 10)) {
+                    textViewCell.setBackgroundResource(R.drawable.green_cell_background); // Highlight cell in green
+                }
+                else {
+                    textViewCell.setBackgroundResource(R.drawable.cell_background);
+                }
             }
 
+            cellBackground.setStroke(2, Color.GRAY);
+            cellBackground.setCornerRadius(8);
+            textViewCell.setElevation(10f);
 
+            // Update total
+            float total = calculateTotal(data);
+            if (activeButton == buttonMoney){
+                totalTextView.setText("$" + total);
+            }
+            if (activeButton == buttonBAC){
+                totalTextView.setText(total + "%");
+            }
+            if (activeButton == buttonCalories){
+                totalTextView.setText(total + " ");
+            }
+            if (activeButton == buttonUnits){
+                totalTextView.setText("#" + total);
+            }
 
             // Grid layout
             GridLayout.LayoutParams params = new GridLayout.LayoutParams();
@@ -155,18 +196,6 @@ public class Monthly_View extends Fragment {
             gridLayoutCalendar.addView(textViewCell);
         }
 
-        highlightActiveButton(activeButton);
-    }
-
-    private void highlightActiveButton(Button highlightButton){
-        // Set all buttons back to default color
-        buttonCalories.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.default_button));
-        buttonUnits.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.default_button));
-        buttonBAC.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.default_button));
-        buttonMoney.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.default_button));
-
-        // Highlight active button
-        highlightButton.setBackgroundTintList(ContextCompat.getColorStateList(getContext(), R.color.active_button));
     }
 
     // Example data for Calories
@@ -187,6 +216,25 @@ public class Monthly_View extends Fragment {
     // Example data for Money
     private float[] getMoneyData() {
         return new float[]{10, 12, 15, 8, 0, 5, 7, 25, 15, 10, 8, 0, 5, 7, 10, 12, 15, 8, 0, 5, 7, 22, 15, 10, 8, 0, 5, 7, 10, 12, 15};
+    }
+
+    // Calculates total number of given metric
+    private float calculateTotal(float[] data) {
+        float total = 0.0f;
+        for (float value : data) {
+            total += value;
+        }
+
+        // Round the total to 2 decimal places
+        total = (Math.round(total * 100f) / 100f);
+
+        return total;
+    }
+
+    // Update images for totals
+    private void updateImageView(int imageResId) {
+
+        imageViewDataType.setImageResource(imageResId);
     }
 
 }
